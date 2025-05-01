42c051725eefa0f16d366e1afce9bdbd26047f0f
iori_n <iori.tanabe@gmail.com>
2025-05-01 23:03:06 +0900

    Added the main program

--- rspls.py
+++ rspls.py
@@ -25,8 +25,20 @@ def name_to_number(name):
   
     # convert name to number using if/elif/else
     # don't forget to return the result!
+
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "Error: invalid name"         
     
-    pass
     
 def number_to_name(number):
     """
@@ -36,9 +48,20 @@ def number_to_name(number):
     
     # convert number to a name using if/elif/else
     # don't forget to return the result!
-    
-    pass
 
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        return "Error: Invalid number"
+ 
 
 def rpsls(player_choice):
     """
@@ -47,23 +70,35 @@ def rpsls(player_choice):
     """
     
     # print a blank line to separate consecutive games
-    
+    print()
+        
     # print out the message for the player's choice
+    print("Player's choice is " + player_choice + ".")
 
     # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
 
     # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0, 4)
 
     # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
     
     # print out message for computer's choice
+    print("The computer's choice is " + comp_choice + ".")
 
     # compute difference of player_number and comp_number modulo five
+    diff = (player_number - comp_number) % 5
+    #print(diff)
 
     # use if/elif/else to determine winner and print winner message
-    
-    pass
-     
+    if diff == 0:
+        print("Player and computer tie!")
+    elif (diff == 1) or (diff == 2):
+        print("Player wins!") 
+    else:
+        print("Computer wins!")    
+
     
 # test your code
 rpsls("rock")
